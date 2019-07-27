b4106fe90a4b819a557d9cbd51e99dc5c950d774
shirai ryohei <shirai.ryohei0302@gmail.com>
2019-07-27 16:07:38 +0900

    トップページを修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,36 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt-blog-test
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js blog test.
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <div>
+    <section class="hero is-medium is-primary is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Welcome to the JavaScript SSR Blog.
+          </h1>
+          <h2 class="subtitle">
+            Hope you find something you like.
+          </h2>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+    
+    <posts />
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-  components: {
-    AppLogo
+  import Posts from '~/components/Posts.vue'
+    
+  export default {
+    components: {
+      Posts
+    },
+    head: {
+      title: 'Home'
+    }
   }
-}
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
 
